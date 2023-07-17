912a4f43533d8ba33b0e84d987bdf8cad474d9c9
Huanyi Chen <huanyi.chen@uwaterloo.ca>
2023-07-17 01:40:13 -0400

    minor fix

--- tut/week10/double_barrier_example/FakeStorageNode.java
+++ tut/week10/double_barrier_example/FakeStorageNode.java
@@ -3,8 +3,8 @@ public class FakeStorageNode {
         // the node should be killed every 2s
         // but if something happens, it will stop in at most 10s
         for (int i = 0; i < 10; i++) {
-        System.out.println("The fake storage node is running");
-        Thread.sleep(1000);
+            System.out.println("The fake storage node is running");
+            Thread.sleep(1000);
         }
         System.out.println("The fake storage node stopped");
     }
